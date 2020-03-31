37fd9331a23edcb1e6b1461ff52b00ea2a63a667
Harshit Kakkar <kharshit0@gmail.com>
2020-03-31 20:43:48 +0530

    API Integration of login page and redirection to home page.

--- src/components/Login/index.tsx
+++ src/components/Login/index.tsx
@@ -1,23 +1,59 @@
 import React from 'react'
-import {Link} from 'react-router-dom'
+import axios from 'axios'
+import {useForm} from 'react-hook-form'
 import './styles.css'
 
-const Login = () => {
+
+
+const Login = (props : any) => {
+
+    const { handleSubmit, register} = useForm();
+
+    const onSubmit = (data: any) => {
+        axios.post('http://localhost:5000/login', data)
+            .then(response => {
+              
+                if (response.status = 200){   
+                    props.history.push("/main/home")
+                }
+            })
+            .catch(error => {
+                alert("Incorrect Username/Password")
+            })
+    }
+
+
     return (
         <div className="login-container">
             <div className="login">
-                <div className="login-head">
-                    Login
-                </div>
-                <div ><input className="input-phone" type="text" placeholder="Phone"/></div>
-                <br></br>
-                <div><input  className="input-password" type="password" placeholder="password"/></div>
-                <br></br>
-                <Link to='/main/home'>
-                    <button className="login-button">
+               <form onSubmit={handleSubmit(onSubmit)}>
+                    <div className="login-head">
                         Login
-                    </button>
-                </Link>
+                    </div>
+                    <div >
+                        <input
+                            className="input-phone" 
+                            type="text" 
+                            placeholder="phone"
+                            name="phone"
+                            ref={register}
+                        />
+                    </div>
+                    <br></br>
+                    <div>
+                        <input 
+                            className="input-password" 
+                            type="password" 
+                            placeholder="password"
+                            name="password"
+                            ref={register}
+                        />
+                    </div>
+                    <br></br>
+                        <button className="login-button">
+                            Login
+                        </button>
+               </form>
             </div>
 
             
