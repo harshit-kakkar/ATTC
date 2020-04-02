c2d5b64d2dfe5c8fd95e3b1678d5e6c9b03a7dfe
Harshit Kakkar <kharshit0@gmail.com>
2020-04-02 16:36:02 +0530

    Temporary fix for continuous  call to server.

--- src/components/Homepage/index.tsx
+++ src/components/Homepage/index.tsx
@@ -3,23 +3,36 @@ import axios from 'axios'
 import './styles.css'
 
 
+var x = 0;
+
 
 
 const Homepage = (props : any) => {
-    // var Name: string = 'Harshit'
+    var Name: string = '1'
     // let balanceMoney: number = 300
     const [dataJson, setDataJson] = React.useState({"name": "User", "balance" : "Undefined"})
+    const changeData = ((newData: any) => {
+        if ((newData["name"] === dataJson["name"]) && (newData["balance"] === dataJson["balance"])){
+            console.log("Names are same")
+        }
+        else{
+            console.log("Names are different")
+            setDataJson(newData)
+        }
+        
+    })
 
     var phone_no = props.phone
     var phone_json = {"phone" : phone_no}
 
 
-    // const proxyurl = "https://cors-anywhere.herokuapp.com/";
     const url = "http://localhost:5000/home";
     axios.post(url, phone_json)
             .then(response => {
-                console.log(response.data["name"])
-                setDataJson(response.data)
+                console.log(response.data)
+                x = x+1;
+                console.log(x) 
+                changeData(response.data)
             })
             .catch(error => {
                 console.log(error)
