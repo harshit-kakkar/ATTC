6622d86214d095f979dd172a3d492a2a2343c6c8
Priyank <priyankmadan98@gmail.com>
2020-04-18 15:31:17 +0530

    used OCR API

--- src/components/Toll/index.tsx
+++ src/components/Toll/index.tsx
@@ -1,27 +1,29 @@
 import React from 'react'
 import axios from 'axios'
 import './styles.css'
-import {useForm} from 'react-hook-form'
+import { useForm } from 'react-hook-form'
 
 
 const Toll = () => {
 
+    const [image, setImage] = React.useState({ preview: '', raw: '' })
+
     const [indication, setIndication] = React.useState(false)
 
-    const { handleSubmit, register} = useForm();
+    const { handleSubmit, register } = useForm();
 
     const url = 'https://attc-backend.herokuapp.com/toll-crossed';
     const onSubmit = (data: any) => {
 
-                    //IMPORTANT
-            data["toll"] = 1
-                    // here taking the toll id to be 1 for simplicity.
-                    // Can be changed later when login is created for toll .
-        
+        //IMPORTANT
+        data["toll"] = 1
+        // here taking the toll id to be 1 for simplicity.
+        // Can be changed later when login is created for toll .
+
         console.log(data)
         axios.post(url, data)
             .then(response => {
-                if(response.status === 200){
+                if (response.status === 200) {
                     setIndication(true)
 
                     //After 5 seconds the green indication background will again turn red for another vehicle.
@@ -32,11 +34,59 @@ const Toll = () => {
             })
             .catch(error => {
                 console.log(error)
-                
-                
+
+
             })
     }
 
+    const handleChange = (e: any) => {
+        setImage({
+            preview: URL.createObjectURL(e.target.files[0]),
+
+            raw: e.target.files[0]
+        })
+    }
+
+    const machineLeaning = () => {
+
+        // const formData = new FormData();
+        // formData.append('url', image.raw)
+        // formData.append('lang', 'eng')
+
+
+
+        axios.post('https://api.ocr.space/parse/image',
+            {
+                body: { 'url': { image: image.raw }, 'language': 'eng' }
+
+            },
+            {
+                headers: { 'apikey': 'helloworld' },
+            })
+            .then(response => {
+                // console.log(response)
+                console.log(response.data)
+            })
+            .catch(error => {
+                console.log(error)
+
+
+            })
+
+        console.log(image.raw)
+        // var reader = new FileReader();
+        // reader.readAsDataURL(); 
+        // reader.onloadend = function() {
+        //     var base64data = reader.result;                
+        //     console.log(base64data);
+        // }
+
+
+        console.log(image.preview)
+        let x = image.raw
+        console.log(typeof (x))
+
+    }
 
 
 
@@ -51,25 +101,28 @@ const Toll = () => {
 
                         {/* Not used ref for uploading image since it is still unclear as to how image will be processed. */}
 
-                        <input 
-                            className="image1-upload" 
+                        <input
+                            className="image1-upload"
                             type="file"
                             name="car_image"
-                            
-                            >
+                            onChange={handleChange}
+
+
+                        >
 
                         </input>
+                        <button onClick={machineLeaning} >Apply ML</button>
                     </div>
                     <div>
 
 
                         {/* Providing the option to write the car number for testing purpose. Should be removed later when image input works. */}
-                        <input 
-                            className="toll-car-number-manual-input" 
-                            placeholder="Car Number" 
+                        <input
+                            className="toll-car-number-manual-input"
+                            placeholder="Car Number"
                             name="car_number"
                             ref={register}
-                            >
+                        >
 
                         </input>
                     </div>
@@ -79,7 +132,7 @@ const Toll = () => {
                     </button>
                 </form>
             </div>
-            <div className={!indication? "toll-indication-off": "toll-indication-on"}>
+            <div className={!indication ? "toll-indication-off" : "toll-indication-on"}>
                 Indication
             </div>
         </div>
